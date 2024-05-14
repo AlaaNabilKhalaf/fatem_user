ace4e62111b686b947a07fb987f713f282610ceb
mostafamoham3d <mostafa_9bbr1@yahoo.com>
2024-05-15 01:58:41 +0300

    ...

--- lib/Features/Products/presentation/views/products_view.dart
+++ lib/Features/Products/presentation/views/products_view.dart
@@ -3,6 +3,7 @@ import 'package:fatem_users/Features/Categories/data/models/categories_enum.dart
 import 'package:fatem_users/Features/Home/Presentation/Controller/Cubits/Products/products_cubit.dart';
 import 'package:fatem_users/Features/Home/Presentation/Controller/Cubits/Products/products_states.dart';
 import 'package:fatem_users/Features/Products/data/models/product_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,6 +15,7 @@ import '../../../../Core/utils/assets_data.dart';
 import '../../../../Core/widgets/back_button.dart';
 import '../../../../Core/widgets/image_svg.dart';
 import '../../../About/presentation/views/about_view.dart';
+import '../../../ItemPage/Presentation/Views/item_page_view.dart';
 
 class ProductsView extends StatefulWidget {
   final CategoriesEnum categoriesEnum;
@@ -74,88 +76,98 @@ class _ProductsViewState extends State<ProductsView> {
               itemBuilder: (context, index) {
                 return BlocBuilder<ProductsCubit, ProductsStates>(
                     builder: (context, cubit) {
-                  return Stack(
-                    children: [
-                      Container(
-                        // height: 184.h,
-                        // width: 156.w,
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(8.r)),
-                            color: itemCardColor.withOpacity(0.3)),
-                      ),
-
-                      //158.h
-                      Positioned(
-                        top: 8.h,
-                        left: 16.w,
-                        right: 16.w,
-                        bottom: 62.h,
-                        child: Container(
-                          height: 114.h,
-                          width: 124.w,
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ItemPageView(productModel: products[index])));
+                    },
+                    child: Stack(
+                      children: [
+                        Container(
+                          // height: 184.h,
+                          // width: 156.w,
                           decoration: BoxDecoration(
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(6.r)),
-                              color: imageBGCardColor),
-                          child: Padding(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 4.65.h, horizontal: 4.w),
-                              child: Container(
-                                width: 116.w,
-                                height: 104.69.h,
-                                decoration: BoxDecoration(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(3.r)),
-                                    image: const DecorationImage(
-                                      image: AssetImage(AssetsData.itemTemp),
-                                      fit: BoxFit.contain,
-                                    )),
-                              )),
+                                  BorderRadius.all(Radius.circular(8.r)),
+                              color: itemCardColor.withOpacity(0.3)),
                         ),
-                      ),
 
-                      Positioned(
-                        right: isArabic() ? 16.w : null,
-                        left: isArabic() ? null : 16.w,
-                        top: 125.h,
-                        width: 124.w,
-                        child: RegularText(
-                            textAlign: TextAlign.start,
-                            fontSizeAr: 13.sp,
-                            text: products[index].nameEnglish ?? '',
-                            fontSizeEn: 13.sp,
-                            textColor: Colors.black,
-                            fontFamilyAr: arRegular,
-                            fontFamilyEn: enRegular),
-                      ),
+                        //158.h
+                        Positioned(
+                          top: 8.h,
+                          left: 16.w,
+                          right: 16.w,
+                          bottom: 62.h,
+                          child: Container(
+                            height: 114.h,
+                            width: 124.w,
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(6.r)),
+                                color: imageBGCardColor),
+                            child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 4.65.h, horizontal: 4.w),
+                                child: Container(
+                                  width: 116.w,
+                                  height: 104.69.h,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(3.r)),
+                                      image: DecorationImage(
+                                        image: AssetImage(products[index].img),
+                                        fit: BoxFit.contain,
+                                      )),
+                                )),
+                          ),
+                        ),
 
-                      Positioned(
-                          top: 141.h,
-                          right: isArabic() ? 121.w : 16.w,
-                          left: isArabic() ? 16.w : 121.w,
-                          child: GestureDetector(
-                            onTap: () {
-                              context
-                                  .read<ProductsCubit>()
-                                  .favoriteAddOrRemove(products[index], index);
-                            },
-                            child: context
+                        Positioned(
+                          right: isArabic() ? 16.w : null,
+                          left: isArabic() ? null : 16.w,
+                          top: 125.h,
+                          width: 124.w,
+                          child: RegularText(
+                              textAlign: TextAlign.start,
+                              fontSizeAr: 13.sp,
+                              text: products[index].nameEnglish ?? '',
+                              fontSizeEn: 13.sp,
+                              textColor: Colors.black,
+                              fontFamilyAr: arRegular,
+                              fontFamilyEn: enRegular),
+                        ),
+
+                        Positioned(
+                            top: 141.h,
+                            right: isArabic() ? 121.w : 16.w,
+                            left: isArabic() ? 16.w : 121.w,
+                            child: GestureDetector(
+                              onTap: () {
+                                context
                                     .read<ProductsCubit>()
-                                    .favoritesIndex
-                                    .contains(index)
-                                ? SvgImage(
-                                    imagePath: AssetsData.heart,
-                                    width: 17.w,
-                                    height: 17.h,
-                                  )
-                                : SvgImage(
-                                    imagePath: AssetsData.emptyHeart,
-                                    width: 17.w,
-                                    height: 17.h,
-                                  ),
-                          ))
-                    ],
+                                    .favoriteAddOrRemove(
+                                        products[index], index);
+                              },
+                              child: context
+                                      .read<ProductsCubit>()
+                                      .favoritesIndex
+                                      .contains(index)
+                                  ? SvgImage(
+                                      imagePath: AssetsData.heart,
+                                      width: 17.w,
+                                      height: 17.h,
+                                    )
+                                  : SvgImage(
+                                      imagePath: AssetsData.emptyHeart,
+                                      width: 17.w,
+                                      height: 17.h,
+                                    ),
+                            ))
+                      ],
+                    ),
                   );
                 });
               },
